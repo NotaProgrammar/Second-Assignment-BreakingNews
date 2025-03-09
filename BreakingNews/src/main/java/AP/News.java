51c842dff270a5c4e8332188a3c33038c538e0d8
Danial Mehrabi <danial.mehrabi.fix@gmail.com>
2025-03-09 16:15:24 +0330

    News Class DisplayNews completed

--- BreakingNews/src/main/java/AP/News.java
+++ BreakingNews/src/main/java/AP/News.java
@@ -7,4 +7,13 @@ public class News {
     String publishedAt;
     String SourceName;
     String Url;
+    public void DisplayNews()
+    {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Description: " + description);
+        System.out.println("Published At: " + publishedAt);
+        System.out.println("Source Name: " + SourceName);
+        System.out.println("Url: " + Url);
+    }
 }
