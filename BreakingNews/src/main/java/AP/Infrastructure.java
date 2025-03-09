9f318ef8aa9e3f6e431fe962f3b61ac8617e580f
Danial Mehrabi <danial.mehrabi.fix@gmail.com>
2025-03-09 16:16:49 +0330

    JSONArray & JSONObject libraries added

--- BreakingNews/src/main/java/AP/Infrastructure.java
+++ BreakingNews/src/main/java/AP/Infrastructure.java
@@ -6,7 +6,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
 public class Infrastructure {
 
     private final String URL;
